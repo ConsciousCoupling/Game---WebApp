903738d68396aa2e3420256e9609c8cf8aba41ad
ConsciousCoupling <consciouscouplingnow@gmail.com>
2026-02-14 10:47:07 -0800

    hide API

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "intima-date.firebaseapp.com",
+  projectId: "intima-date",
+  storageBucket: "intima-date.firebasestorage.app",
+  messagingSenderId: "1059417189725",
+  appId: "1:1059417189725:web:2dfbd47e2c7d6bef678cb4",
+  measurementId: "G-F8PQ70C0QV"
+};
